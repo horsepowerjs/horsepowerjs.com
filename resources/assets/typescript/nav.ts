b1c1a92ba7b2a9e0d5645e1b17244e4c66ccc7c1
Ryan Naddy <rnaddy@corp.acesse.com>
2019-08-29 17:24:39 -0500

    Added more documentation

--- resources/assets/typescript/nav.ts
+++ resources/assets/typescript/nav.ts
@@ -1,5 +1,8 @@
-let subNav = document.querySelector('#overview-nav')
-if (subNav) {
+import * as url from 'url'
+
+let overviewNav = document.querySelector('#overview-nav')
+let masterNav = document.querySelector('#master-nav')
+if (overviewNav) {
   let tags = [...document.querySelectorAll('#main-content h1, #main-content h2, #main-content h3, #main-content h4, #main-content h5,#main-content h6')]
   let items = tags.map(i => `<li class="nav-${i.tagName.toLowerCase()}"><a href="#${i.textContent.toLowerCase().replace(/\s/g, '-')}">${i.textContent}</a></li>`)
 
@@ -10,6 +13,25 @@ if (subNav) {
   })
 
   let ul = document.createElement('ul')
-  subNav.appendChild(ul)
+  overviewNav.appendChild(ul)
   ul.innerHTML = items.join('')
+}
+
+if (masterNav) {
+  [...masterNav.querySelectorAll('a.header')].forEach((aEl, idx, items) => {
+    if (new RegExp(aEl.getAttribute('pattern') || '').test(window.location.pathname)) {
+      aEl.parentElement.classList.add('visible')
+    }
+    [...aEl.parentElement.querySelectorAll('ul>li>a')].forEach(el => {
+      if (url.parse(el.href).pathname == window.location.pathname) {
+        el.insertAdjacentHTML('beforebegin', '<span class="fa-li fa-xs"><i class="fa-xs fas fa-circle"></i></span>')
+        el.parentElement.classList.add('selected')
+      }
+    })
+    aEl.addEventListener('click', e => {
+      e.preventDefault()
+      items.forEach(cur => cur != aEl && cur.parentElement.classList.remove('visible'))
+      aEl.parentElement.classList.toggle('visible')
+    })
+  })
 }
--- routes/web.js
+++ routes/web.js
@@ -3,5 +3,6 @@ const { Router } = require('@horsepower/router')
 Router.get('/', client => client.response.cached('welcome.mix', 3600)).name('welcome')
 Router.get('/getting-started', client => client.response.cached('getting-started.mix', 3600)).name('getting-started')
 Router.get('/docs', client => client.response.cached('documentation.mix', 3600)).name('docs')
-// Router.get('/docs/mix', client => client.response.cached('documentation.mix', 3600)).name('docs')
 Router.get(/\/docs\/.+/, client => client.response.render('documentation.mix'))
+
+Router.get('/test', client => 123)

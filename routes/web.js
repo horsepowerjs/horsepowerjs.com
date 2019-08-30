c7e4ef1e3cc650a6ac2ad143d2fa4290b591bf63
Ryan Naddy <rnaddy@corp.acesse.com>
2019-08-30 16:12:29 -0500

    Added more documentation

--- routes/web.js
+++ routes/web.js
@@ -1,8 +1,12 @@
 const { Router } = require('@horsepower/router')
+const { Storage } = require('@horsepower/storage')
+const crypto = require('crypto')
 
 Router.get('/', client => client.response.cached('welcome.mix', 3600)).name('welcome')
 Router.get('/getting-started', client => client.response.cached('getting-started.mix', 3600)).name('getting-started')
-Router.get('/docs', client => client.response.cached('documentation.mix', 3600)).name('docs')
-Router.get(/\/docs\/.+/, client => client.response.render('documentation.mix'))
+Router.get(/^\/docs.*/, client => client.response.render('documentation.mix', {
+  md5: (string) => crypto.createHash('md5').update(string).digest('hex'),
+  emails: ['one@example.com', 'two@example.com', 'three@example.com']
+}))
 
-Router.get('/test', client => 123)
+// Router.get('/test', client => client.response.re(Storage.mount('public').toPath('images/logo.svg')))
